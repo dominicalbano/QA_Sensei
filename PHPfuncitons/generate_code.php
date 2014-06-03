91dad830c7960bc02db78735699032c766ed19f2
dominicalbano <dominic.albano@getg5.com>
2014-06-02 21:57:44 -0700

    Create generate_code.php
    php that generates a unique code for each group

---
+++ PHPfuncitons/generate_code.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$possible_values =  array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+
+$code = "";
+$count = 0;
+while($count < 30)
+{
+    
+    $value = array_rand($possible_values);
+    $code = $code . $possible_values[$value];
+    $count = $count + 1;
+}
+echo $code;
+
+?>
